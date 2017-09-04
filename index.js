4540872a34c1928dbfca7e8d4e577d91daa4f8a7
samvnkauffman-pc <samvnkauffman@gmail.com>
2017-09-04 09:24:27 -0400

    add baseline option

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-px-to-prem', function (opts = {}) {
 
+    const defaults = {
+        baseline: 16,
+    };
+
+    opts = opts.assign({}, defaults, opts);
+
+    opts.baseline = parseInt(opts.baseline); // convert e.g. "16px" to 16
+
     return function (root, result) {
 
         // Transform CSS AST here
