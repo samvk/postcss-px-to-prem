8961f0a78ace26a92779f6fb719c9c83454ef229
samvnkauffman-pc <samvnkauffman@gmail.com>
2017-09-04 12:30:58 -0400

    WIP::add regex

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-px-to-prem', function (opts = {}) {
-
+module.exports = postcss.plugin('postcss-px-to-prem', (opts = {}) => {
     const defaults = {
         baseline: 16,
     };
 
     opts = opts.assign({}, defaults, opts);
+    opts.baseline = parseInt(opts.baseline, 10); // convert e.g. "16px" to 16
 
-    opts.baseline = parseInt(opts.baseline); // convert e.g. "16px" to 16
+    return function (css, result) {
 
-    return function (root, result) {
+        css.walkRules((rule) => {
+            const selector = rule.selector;
+
+            selector.replace('');
+
+        });
 
         // Transform CSS AST here
 
