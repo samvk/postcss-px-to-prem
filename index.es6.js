54c137f40bf9c28fa162e18af14c90b51757e98d
Sam Kauffman <samvnkauffman@gmail.com>
2017-09-04 15:04:00 -0400

    babelify index.js

---
+++ index.es6.js
@@ -0,0 +1,20 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-px-to-prem', (opts = {}) => {
+    const defaults = {
+        baseline: 16,
+    };
+
+    opts = { ...defaults, ...opts };
+    opts.baseline = parseInt(opts.baseline, 10); // convert e.g. "16px" to 16
+
+    return (css) => {
+
+        css.walkRules(({ selector }) => {
+            selector.replace(/(\.?\d+(?:\.\d+)?)(pr?em)/, (undefined, value, unit) => {
+                unit = unit.replace('p', '');
+                return value + unit;
+            });
+        });
+    };
+});
--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+'use strict';
+
+var _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i]; for (var key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                target[key] = source[key];
+            }
+        }
+    } return target;
+};
+
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-px-to-prem', (opts = {}) => {
-    const defaults = {
-        baseline: 16,
+module.exports = postcss.plugin('postcss-px-to-prem', function () {
+    var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    var defaults = {
+        baseline: 16
     };
 
-    opts = { ...defaults, ...opts };
+    opts = _extends({}, defaults, opts);
     opts.baseline = parseInt(opts.baseline, 10); // convert e.g. "16px" to 16
 
-    return (css) => {
+    return function (css) {
 
-        css.walkRules(({ selector }) => {
-            selector.replace(/(\.?\d+(?:\.\d+)?)(pr?em)/, (undefined, value, unit) => {
+        css.walkRules(function (_ref) {
+            var selector = _ref.selector;
+
+            selector.replace(/(\.?\d+(?:\.\d+)?)(pr?em)/, function (undefined, value, unit) {
                 unit = unit.replace('p', '');
                 return value + unit;
             });
