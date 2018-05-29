adc9dff625c6bb09244621eced528d29bba66da8
samvnkauffman-pc <samvnkauffman@gmail.com>
2018-05-29 11:03:55 -0400

    rename baseline to rootValue more consistent with other PostCSS repos

--- index.es6.js
+++ index.es6.js
@@ -2,17 +2,17 @@ import postcss from 'postcss';
 
 export default postcss.plugin('postcss-px-to-prem', (opts = {}) => {
     const defaults = {
-        baseline: 16,
+        rootValue: 16,
     };
 
     opts = { ...defaults, ...opts };
 
-    const baseline = parseInt(opts.baseline, 10);
+    const rootValue = parseInt(opts.rootValue, 10);
 
     return (css) => {
 
         css.replaceValues(/\.?\d+(\.\d+)?pr?em/g, { fast: 'em' }, (str) => {
-            const value = parseFloat(str, 10) / baseline; // get number (the value) and divided baseline
+            const value = parseFloat(str, 10) / rootValue; // get number (the value) and divided rootValue
             const unit = str.replace(/[^a-oq-z]/g, ''); // get letters (the unit) and remove 'p'
             return value + unit;
         });
