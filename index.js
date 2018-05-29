7f36e10e6eec4baf3f4f905dac35d8e69eecbfa3
samvnkauffman-pc <samvnkauffman@gmail.com>
2018-05-29 11:48:24 -0400

    remove unnecessary ESNext support changes

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-'use strict';
-
-var _extends = Object.assign || function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-        var source = arguments[i]; for (var key in source) {
-            if (Object.prototype.hasOwnProperty.call(source, key)) {
-                target[key] = source[key];
-            }
-        }
-    } return target;
-};
-
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-px-to-prem', (opts = {}) => {
     const defaults = {
-        rootValue: 16
+        rootValue: 16,
     };
 
-    opts = _extends({}, defaults, opts);
+    opts = Object.assign({}, defaults, opts);
 
     const rootValue = parseInt(opts.rootValue, 10);
 
-    return css => {
+    return (css) => {
 
-        css.replaceValues(/\.?\d+(\.\d+)?pr?em/g, { fast: 'em' }, str => {
+        css.replaceValues(/\.?\d+(\.\d+)?pr?em/g, { fast: 'em' }, (str) => {
             const value = (parseFloat(str, 10) / rootValue).toFixed(3); // get number (the value) and divided rootValue
             const unit = str.replace(/[^a-oq-z]/g, ''); // get letters (the unit) and remove 'p'
             return value + unit;
